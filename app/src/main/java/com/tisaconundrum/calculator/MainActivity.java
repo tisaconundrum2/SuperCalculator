c3d7cb4d6d3dc87c8be9a11289c45bbd3294dd8c
Nicholas Finch <ngf4@nau.edu>
2016-10-03 17:01:20 -0700

    Trying some code I found online...
    This may or may not work. Rebase if it doesn't!
    This is the website I got the code from
    http://mrbool.com/how-to-create-a-calculator-app-for-android/28100

--- app/src/main/java/com/tisaconundrum/calculator/MainActivity.java
+++ app/src/main/java/com/tisaconundrum/calculator/MainActivity.java
@@ -9,22 +9,71 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    private EditText op1, op2;
-    private Button btnAdd, btnSub, btnMul, btnDiv;
+import static android.R.attr.x;
+import static com.tisaconundrum.calculator.R.id.btnAdd;
+import static com.tisaconundrum.calculator.R.id.btnMul;
+import static com.tisaconundrum.calculator.R.id.btnSub;
+import static com.tisaconundrum.calculator.R.id.op1;
+import static com.tisaconundrum.calculator.R.id.op2;
+
+
+public abstract class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText disp;
+    private Button
+            btnClear,
+            btnParen,
+            btnPercent,
+            btnDiv,
+            btnOne,
+            btnTwo,
+            btnThree,
+            btnMul,
+            btnFour,
+            btnFive,
+            btnSix,
+            btnSub,
+            btnSeven,
+            btnEight,
+            btnNine,
+            btnAdd,
+            btnDot,
+            btnZero,
+            btnPlusMinus,
+            btnEqual;
     private TextView txtResult;
+    int op1, op2;
+    String optr;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        op1 = (EditText) findViewById(R.id.op1);
-        op2 = (EditText) findViewById(R.id.op2);
+        op1 = (EditText) findViewById(op1);
+        op2 = (EditText) findViewById(op2);
 
-        btnAdd = (Button) findViewById(R.id.btnAdd);
-        btnSub = (Button) findViewById(R.id.btnSub);
-        btnMul = (Button) findViewById(R.id.btnMul);
-        btnDiv = (Button) findViewById(R.id.btnDiv);
-        txtResult = (TextView) findViewById(R.id.txtResult);
+        txtResult    = (TextView) findViewById(R.id.txtResult);
+        btnClear     = (Button) findViewById(R.id.btnClear);
+        btnParen     = (Button) findViewById(R.id.btnParen);
+        btnPercent   = (Button) findViewById(R.id.btnPercent);
+        btnDiv       = (Button) findViewById(R.id.btnDiv);
+        btnOne       = (Button) findViewById(R.id.btnOne);
+        btnTwo       = (Button) findViewById(R.id.btnTwo);
+        btnThree     = (Button) findViewById(R.id.btnThree);
+        btnMul       = (Button) findViewById(R.id.btnMul);
+        btnFour      = (Button) findViewById(R.id.btnFour);
+        btnFive      = (Button) findViewById(R.id.btnFive);
+        btnSix       = (Button) findViewById(R.id.btnSix);
+        btnSub       = (Button) findViewById(R.id.btnSub);
+        btnSeven     = (Button) findViewById(R.id.btnSeven);
+        btnEight     = (Button) findViewById(R.id.btnEight);
+        btnNine      = (Button) findViewById(R.id.btnNine);
+        btnAdd       = (Button) findViewById(R.id.btnAdd);
+        btnDot       = (Button) findViewById(R.id.btnDot);
+        btnZero      = (Button) findViewById(R.id.btnZero);
+        btnPlusMinus = (Button) findViewById(R.id.btnPlusMinus);
+        btnEqual     = (Button) findViewById(R.id.btnEqual);
+
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
