ae8bbb45743d77ab9ffa895720c4ecadcaae48a4
Nicholas Finch <ngf4@nau.edu>
2016-10-03 20:42:45 -0700

    Fix

--- app/src/main/java/com/tisaconundrum/calculator/MainActivity.java
+++ app/src/main/java/com/tisaconundrum/calculator/MainActivity.java
@@ -1,11 +1,10 @@
+package com.tisaconundrum.calculator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import com.tisaconundrum.calculator.R;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -18,7 +17,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
 
         one = (Button) findViewById(R.id.one);
         two = (Button) findViewById(R.id.two);
